f3534897db3df8229d8f89b719a3e12f75f0b59d
hoenn <evan.mena13@gmail.com>
2019-06-11 12:41:00 -0400

    Don't close db immediately, correctly handle subcmd flags, add signal loop

--- cmd/hnfilter-cli/cmd/root.go
+++ cmd/hnfilter-cli/cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/hoenn/hnfilter/pkg/store"
 
@@ -20,7 +22,8 @@ var (
 	dbPort string
 	dbName string
 
-	ds *store.DataStore
+	ds       *store.DataStore
+	database *sql.DB
 )
 
 func init() {
@@ -33,9 +36,11 @@ func init() {
 	rootCmd.Flags().StringVarP(&dbPort, "dbport", "P", "", "postgres database port")
 	rootCmd.MarkFlagRequired("dbport")
 	rootCmd.Flags().StringVarP(&dbName, "dbname", "n", "", "postgres database name")
+	rootCmd.MarkFlagRequired("dbname")
 }
 
 var rootCmd = &cobra.Command{
+	TraverseChildren: true,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		dbConn := &store.DBConn{
 			Username: dbUser,
@@ -45,16 +50,16 @@ var rootCmd = &cobra.Command{
 			Name:     dbName,
 		}
 		db, err := sql.Open("postgres", dbConn.Format())
+		database = db
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer db.Close()
-		err = db.Ping()
+		err = database.Ping()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		ds = store.NewDataStore(db)
+		ds = store.NewDataStore(database)
 
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -64,8 +69,20 @@ var rootCmd = &cobra.Command{
 
 //Execute the command
 func Execute() {
+	go func() {
+		<-waitForSignal()
+		fmt.Println("caught signal, closing")
+		database.Close()
+
+	}()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func waitForSignal() chan os.Signal {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP)
+	return c
 }
